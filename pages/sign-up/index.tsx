d3e0f80078783193cc2d561c537e14983d86a377
dones <86201269+adonesbalica@users.noreply.github.com>
2026-02-17 14:51:10 +0000

    feat: create sign-in and sign-up pages without flux off login

--- pages/sign-up/index.tsx
+++ pages/sign-up/index.tsx
@@ -1,8 +1,8 @@
-import { LoginForm } from "@/components/login-form";
 import { Logo } from "@/components/logo";
+import { SignUpForm } from "@/components/sign-up-form";
 import Link from "next/link";
 
-export default function SignInPage() {
+export default function SignUpPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-6 bg-(--background) px-4">
       <div className="flex flex-col items-center justify-center gap-4 text-center">
@@ -14,19 +14,19 @@ export default function SignInPage() {
 
       <div className="w-full max-w-md rounded-xl border border-(--border) bg-(--card) p-8 shadow-2xl">
         <h1 className="text-2xl font-semibold text-(--foreground)">
-          Welcome back
+          Create you account
         </h1>
 
         <p className="mt-2 text-sm text-(--muted-foreground)">
-          Sign in to manage your wishlist.
+          Sign up to manage your wishlist.
         </p>
 
-        <LoginForm />
+        <SignUpForm />
 
         <p className="mt-6 text-center text-sm text-(--muted-foreground)">
-          Don&apos;t have an account?{" "}
-          <Link href="/sign-up" className="cursor-pointer text-(--primary) hover:opacity-80">
-            Create one
+          Already have an account?{" "}
+          <Link href="/sign-in" className="cursor-pointer text-(--primary) hover:opacity-80">
+            Sing In
           </Link>
         </p>
       </div>
